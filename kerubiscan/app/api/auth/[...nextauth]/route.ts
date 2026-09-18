4792d9ee821beb4235eccfab83abb35e5830cfa3
Ngoh1919 <aghet87@gmail.com>
2026-09-18 14:56:52 +0100

    Added missing PKCE checks to NextAuth

--- kerubiscan/app/api/auth/[...nextauth]/route.ts
+++ kerubiscan/app/api/auth/[...nextauth]/route.ts
@@ -19,7 +19,6 @@ function getDynamicAuthOptions(req?: NextRequest): AuthOptions {
         type: "oauth",
         version: "2.0",
         checks: ["pkce", "state"],
-        idToken: true,
         clientId: process.env.KEYCLOAK_CLIENT_ID || "kerubiscan-web",
         clientSecret: process.env.KEYCLOAK_CLIENT_SECRET || "kerubiscan-web-secret",
         authorization: {
